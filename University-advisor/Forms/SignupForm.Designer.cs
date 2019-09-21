f13352e049e3e1052eabd6f60474d29dafaf653f
Jaroslav Avižen <avizen.j@gmail.com>
2019-09-22 00:36:19 +0300

    Adjusted Sign Up window design.

--- University-advisor/Forms/SignupForm.Designer.cs
+++ University-advisor/Forms/SignupForm.Designer.cs
@@ -43,13 +43,15 @@
             this.universityBox = new System.Windows.Forms.ComboBox();
             this.statusBox = new System.Windows.Forms.ComboBox();
             this.createAccountButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // firstNameLabel
             // 
             this.firstNameLabel.AutoSize = true;
             this.firstNameLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.firstNameLabel.Location = new System.Drawing.Point(12, 20);
+            this.firstNameLabel.Location = new System.Drawing.Point(12, 69);
             this.firstNameLabel.Name = "firstNameLabel";
             this.firstNameLabel.Size = new System.Drawing.Size(86, 21);
             this.firstNameLabel.TabIndex = 0;
@@ -59,7 +61,7 @@
             // 
             this.lastNameLabel.AutoSize = true;
             this.lastNameLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lastNameLabel.Location = new System.Drawing.Point(12, 50);
+            this.lastNameLabel.Location = new System.Drawing.Point(243, 69);
             this.lastNameLabel.Name = "lastNameLabel";
             this.lastNameLabel.Size = new System.Drawing.Size(83, 21);
             this.lastNameLabel.TabIndex = 1;
@@ -67,23 +69,23 @@
             // 
             // firstNameText
             // 
-            this.firstNameText.Location = new System.Drawing.Point(104, 20);
+            this.firstNameText.Location = new System.Drawing.Point(16, 93);
             this.firstNameText.Name = "firstNameText";
-            this.firstNameText.Size = new System.Drawing.Size(161, 22);
+            this.firstNameText.Size = new System.Drawing.Size(198, 22);
             this.firstNameText.TabIndex = 2;
             // 
             // lastNameText
             // 
-            this.lastNameText.Location = new System.Drawing.Point(104, 50);
+            this.lastNameText.Location = new System.Drawing.Point(247, 93);
             this.lastNameText.Name = "lastNameText";
-            this.lastNameText.Size = new System.Drawing.Size(161, 22);
+            this.lastNameText.Size = new System.Drawing.Size(198, 22);
             this.lastNameText.TabIndex = 3;
             // 
             // emailLabel
             // 
             this.emailLabel.AutoSize = true;
             this.emailLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailLabel.Location = new System.Drawing.Point(12, 81);
+            this.emailLabel.Location = new System.Drawing.Point(243, 20);
             this.emailLabel.Name = "emailLabel";
             this.emailLabel.Size = new System.Drawing.Size(48, 21);
             this.emailLabel.TabIndex = 4;
@@ -91,16 +93,16 @@
             // 
             // emailText
             // 
-            this.emailText.Location = new System.Drawing.Point(104, 80);
+            this.emailText.Location = new System.Drawing.Point(247, 44);
             this.emailText.Name = "emailText";
-            this.emailText.Size = new System.Drawing.Size(161, 22);
+            this.emailText.Size = new System.Drawing.Size(198, 22);
             this.emailText.TabIndex = 5;
             // 
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
             this.passwordLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordLabel.Location = new System.Drawing.Point(12, 129);
+            this.passwordLabel.Location = new System.Drawing.Point(12, 202);
             this.passwordLabel.Name = "passwordLabel";
             this.passwordLabel.Size = new System.Drawing.Size(77, 21);
             this.passwordLabel.TabIndex = 6;
@@ -108,16 +110,16 @@
             // 
             // passwordText
             // 
-            this.passwordText.Location = new System.Drawing.Point(16, 153);
+            this.passwordText.Location = new System.Drawing.Point(16, 226);
             this.passwordText.Name = "passwordText";
-            this.passwordText.Size = new System.Drawing.Size(249, 22);
+            this.passwordText.Size = new System.Drawing.Size(198, 22);
             this.passwordText.TabIndex = 7;
             // 
             // confirmPasswordLabel
             // 
             this.confirmPasswordLabel.AutoSize = true;
             this.confirmPasswordLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.confirmPasswordLabel.Location = new System.Drawing.Point(12, 178);
+            this.confirmPasswordLabel.Location = new System.Drawing.Point(12, 251);
             this.confirmPasswordLabel.Name = "confirmPasswordLabel";
             this.confirmPasswordLabel.Size = new System.Drawing.Size(137, 21);
             this.confirmPasswordLabel.TabIndex = 8;
@@ -125,16 +127,16 @@
             // 
             // confirmPasswordText
             // 
-            this.confirmPasswordText.Location = new System.Drawing.Point(16, 202);
+            this.confirmPasswordText.Location = new System.Drawing.Point(16, 275);
             this.confirmPasswordText.Name = "confirmPasswordText";
-            this.confirmPasswordText.Size = new System.Drawing.Size(249, 22);
+            this.confirmPasswordText.Size = new System.Drawing.Size(198, 22);
             this.confirmPasswordText.TabIndex = 9;
             // 
             // universityLabel
             // 
             this.universityLabel.AutoSize = true;
             this.universityLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.universityLabel.Location = new System.Drawing.Point(291, 20);
+            this.universityLabel.Location = new System.Drawing.Point(12, 118);
             this.universityLabel.Name = "universityLabel";
             this.universityLabel.Size = new System.Drawing.Size(81, 21);
             this.universityLabel.TabIndex = 10;
@@ -144,7 +146,7 @@
             // 
             this.statusLabel.AutoSize = true;
             this.statusLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusLabel.Location = new System.Drawing.Point(291, 51);
+            this.statusLabel.Location = new System.Drawing.Point(243, 118);
             this.statusLabel.Name = "statusLabel";
             this.statusLabel.Size = new System.Drawing.Size(54, 21);
             this.statusLabel.TabIndex = 11;
@@ -152,35 +154,59 @@
             // 
             // universityBox
             // 
+            this.universityBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.universityBox.DropDownWidth = 500;
             this.universityBox.FormattingEnabled = true;
-            this.universityBox.Location = new System.Drawing.Point(391, 17);
+            this.universityBox.IntegralHeight = false;
+            this.universityBox.Location = new System.Drawing.Point(16, 142);
             this.universityBox.Name = "universityBox";
-            this.universityBox.Size = new System.Drawing.Size(108, 24);
+            this.universityBox.Size = new System.Drawing.Size(198, 24);
             this.universityBox.TabIndex = 12;
             // 
             // statusBox
             // 
+            this.statusBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.statusBox.FormattingEnabled = true;
-            this.statusBox.Location = new System.Drawing.Point(391, 48);
+            this.statusBox.Location = new System.Drawing.Point(247, 142);
             this.statusBox.Name = "statusBox";
-            this.statusBox.Size = new System.Drawing.Size(108, 24);
+            this.statusBox.Size = new System.Drawing.Size(198, 24);
             this.statusBox.TabIndex = 13;
             // 
             // createAccountButton
             // 
-            this.createAccountButton.Location = new System.Drawing.Point(391, 178);
+            this.createAccountButton.Location = new System.Drawing.Point(16, 333);
             this.createAccountButton.Margin = new System.Windows.Forms.Padding(0);
             this.createAccountButton.Name = "createAccountButton";
-            this.createAccountButton.Size = new System.Drawing.Size(108, 46);
+            this.createAccountButton.Size = new System.Drawing.Size(198, 46);
             this.createAccountButton.TabIndex = 14;
             this.createAccountButton.Text = "Create new account";
             this.createAccountButton.UseVisualStyleBackColor = true;
+            this.createAccountButton.Click += new System.EventHandler(this.CreateAccountButton_Click);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(12, 20);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(81, 21);
+            this.usernameLabel.TabIndex = 15;
+            this.usernameLabel.Text = "Username";
+            // 
+            // usernameText
+            // 
+            this.usernameText.Location = new System.Drawing.Point(16, 44);
+            this.usernameText.Name = "usernameText";
+            this.usernameText.Size = new System.Drawing.Size(198, 22);
+            this.usernameText.TabIndex = 16;
             // 
             // SignupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 248);
+            this.ClientSize = new System.Drawing.Size(464, 396);
+            this.Controls.Add(this.usernameText);
+            this.Controls.Add(this.usernameLabel);
             this.Controls.Add(this.createAccountButton);
             this.Controls.Add(this.statusBox);
             this.Controls.Add(this.universityBox);
@@ -220,5 +246,7 @@
         private System.Windows.Forms.ComboBox universityBox;
         private System.Windows.Forms.ComboBox statusBox;
         private System.Windows.Forms.Button createAccountButton;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameText;
     }
 }
