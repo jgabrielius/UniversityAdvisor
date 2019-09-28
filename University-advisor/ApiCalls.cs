4116160edabf576ce33e674c258718176058aa59
tama5682 <tadas.marcinkevicius@mif.stud.vu.lt>
2019-09-28 16:59:50 +0300

    Added Location model, Geocoding api integration

---
+++ University-advisor/ApiCalls.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace University_advisor
+{
+    class ApiCalls {
+
+        public string GetLocationJson(string url) {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch(WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    string errorText = reader.ReadToEnd();
+                    //TO DO: Log error
+                }
+                throw;
+            }
+        }
+
+    }
+}
