a84c02528f8981ee20d7e345630d924ec73d3819
Tadas-M <tadoam@gmail.com>
2019-10-02 19:38:37 +0300

    Refactored GMap code, fixed issues related to GMap

--- University-advisor/Forms/SignupForm.Designer.cs
+++ University-advisor/Forms/SignupForm.Designer.cs
@@ -51,39 +51,44 @@
             // 
             // firstNameText
             // 
-            this.firstNameText.Location = new System.Drawing.Point(273, 172);
+            this.firstNameText.Location = new System.Drawing.Point(205, 140);
+            this.firstNameText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.firstNameText.Name = "firstNameText";
-            this.firstNameText.Size = new System.Drawing.Size(238, 22);
+            this.firstNameText.Size = new System.Drawing.Size(180, 20);
             this.firstNameText.TabIndex = 2;
             // 
             // lastNameText
             // 
-            this.lastNameText.Location = new System.Drawing.Point(273, 216);
+            this.lastNameText.Location = new System.Drawing.Point(205, 176);
+            this.lastNameText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lastNameText.Name = "lastNameText";
-            this.lastNameText.Size = new System.Drawing.Size(238, 22);
+            this.lastNameText.Size = new System.Drawing.Size(180, 20);
             this.lastNameText.TabIndex = 3;
             // 
             // emailText
             // 
-            this.emailText.Location = new System.Drawing.Point(273, 124);
+            this.emailText.Location = new System.Drawing.Point(205, 101);
+            this.emailText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.emailText.Name = "emailText";
-            this.emailText.Size = new System.Drawing.Size(238, 22);
+            this.emailText.Size = new System.Drawing.Size(180, 20);
             this.emailText.TabIndex = 5;
             // 
             // passwordText
             // 
-            this.passwordText.Location = new System.Drawing.Point(273, 365);
+            this.passwordText.Location = new System.Drawing.Point(205, 297);
+            this.passwordText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.passwordText.Name = "passwordText";
             this.passwordText.PasswordChar = '*';
-            this.passwordText.Size = new System.Drawing.Size(238, 22);
+            this.passwordText.Size = new System.Drawing.Size(180, 20);
             this.passwordText.TabIndex = 7;
             // 
             // confirmPasswordText
             // 
-            this.confirmPasswordText.Location = new System.Drawing.Point(273, 412);
+            this.confirmPasswordText.Location = new System.Drawing.Point(205, 335);
+            this.confirmPasswordText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.confirmPasswordText.Name = "confirmPasswordText";
             this.confirmPasswordText.PasswordChar = '*';
-            this.confirmPasswordText.Size = new System.Drawing.Size(238, 22);
+            this.confirmPasswordText.Size = new System.Drawing.Size(180, 20);
             this.confirmPasswordText.TabIndex = 9;
             // 
             // universityBox
@@ -93,25 +98,28 @@
             this.universityBox.DropDownWidth = 500;
             this.universityBox.FormattingEnabled = true;
             this.universityBox.IntegralHeight = false;
-            this.universityBox.Location = new System.Drawing.Point(273, 262);
+            this.universityBox.Location = new System.Drawing.Point(205, 213);
+            this.universityBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.universityBox.Name = "universityBox";
-            this.universityBox.Size = new System.Drawing.Size(238, 24);
+            this.universityBox.Size = new System.Drawing.Size(180, 21);
             this.universityBox.TabIndex = 12;
             // 
             // statusBox
             // 
             this.statusBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.statusBox.FormattingEnabled = true;
-            this.statusBox.Location = new System.Drawing.Point(273, 313);
+            this.statusBox.Location = new System.Drawing.Point(205, 254);
+            this.statusBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.statusBox.Name = "statusBox";
-            this.statusBox.Size = new System.Drawing.Size(238, 24);
+            this.statusBox.Size = new System.Drawing.Size(180, 21);
             this.statusBox.TabIndex = 13;
             // 
             // usernameText
             // 
-            this.usernameText.Location = new System.Drawing.Point(273, 75);
+            this.usernameText.Location = new System.Drawing.Point(205, 61);
+            this.usernameText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.usernameText.Name = "usernameText";
-            this.usernameText.Size = new System.Drawing.Size(238, 22);
+            this.usernameText.Size = new System.Drawing.Size(180, 20);
             this.usernameText.TabIndex = 16;
             // 
             // usernameLabel
@@ -119,9 +127,10 @@
             this.usernameLabel.AutoSize = true;
             this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.usernameLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.usernameLabel.Location = new System.Drawing.Point(132, 71);
+            this.usernameLabel.Location = new System.Drawing.Point(99, 58);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(135, 25);
+            this.usernameLabel.Size = new System.Drawing.Size(109, 20);
             this.usernameLabel.TabIndex = 17;
             this.usernameLabel.Text = "USERNAME: ";
             this.usernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -131,9 +140,10 @@
             this.emailLabel.AutoSize = true;
             this.emailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.emailLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.emailLabel.Location = new System.Drawing.Point(184, 120);
+            this.emailLabel.Location = new System.Drawing.Point(138, 98);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(83, 25);
+            this.emailLabel.Size = new System.Drawing.Size(66, 20);
             this.emailLabel.TabIndex = 18;
             this.emailLabel.Text = "EMAIL: ";
             this.emailLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -143,9 +153,10 @@
             this.firstNameLabel.AutoSize = true;
             this.firstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.firstNameLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.firstNameLabel.Location = new System.Drawing.Point(124, 168);
+            this.firstNameLabel.Location = new System.Drawing.Point(93, 136);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.firstNameLabel.Name = "firstNameLabel";
-            this.firstNameLabel.Size = new System.Drawing.Size(143, 25);
+            this.firstNameLabel.Size = new System.Drawing.Size(114, 20);
             this.firstNameLabel.TabIndex = 19;
             this.firstNameLabel.Text = "FIRST NAME: ";
             this.firstNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -155,9 +166,10 @@
             this.lastNameLabel.AutoSize = true;
             this.lastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.lastNameLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.lastNameLabel.Location = new System.Drawing.Point(129, 213);
+            this.lastNameLabel.Location = new System.Drawing.Point(97, 173);
+            this.lastNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lastNameLabel.Name = "lastNameLabel";
-            this.lastNameLabel.Size = new System.Drawing.Size(138, 25);
+            this.lastNameLabel.Size = new System.Drawing.Size(107, 20);
             this.lastNameLabel.TabIndex = 20;
             this.lastNameLabel.Text = "LAST NAME: ";
             this.lastNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -167,9 +179,10 @@
             this.universityLabel.AutoSize = true;
             this.universityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.universityLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.universityLabel.Location = new System.Drawing.Point(124, 262);
+            this.universityLabel.Location = new System.Drawing.Point(93, 213);
+            this.universityLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.universityLabel.Name = "universityLabel";
-            this.universityLabel.Size = new System.Drawing.Size(136, 25);
+            this.universityLabel.Size = new System.Drawing.Size(111, 20);
             this.universityLabel.TabIndex = 21;
             this.universityLabel.Text = "UNIVERSITY:";
             this.universityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -179,9 +192,10 @@
             this.statusLabel.AutoSize = true;
             this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.statusLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.statusLabel.Location = new System.Drawing.Point(160, 313);
+            this.statusLabel.Location = new System.Drawing.Point(120, 254);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(100, 25);
+            this.statusLabel.Size = new System.Drawing.Size(76, 20);
             this.statusLabel.TabIndex = 22;
             this.statusLabel.Text = "STATUS:";
             this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -191,9 +205,10 @@
             this.passwordLabel.AutoSize = true;
             this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.passwordLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.passwordLabel.Location = new System.Drawing.Point(124, 365);
+            this.passwordLabel.Location = new System.Drawing.Point(93, 297);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(136, 25);
+            this.passwordLabel.Size = new System.Drawing.Size(107, 20);
             this.passwordLabel.TabIndex = 23;
             this.passwordLabel.Text = "PASSWORD:";
             this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -203,9 +218,10 @@
             this.confirmPasswordLabel.AutoSize = true;
             this.confirmPasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.confirmPasswordLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.confirmPasswordLabel.Location = new System.Drawing.Point(27, 409);
+            this.confirmPasswordLabel.Location = new System.Drawing.Point(20, 332);
+            this.confirmPasswordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.confirmPasswordLabel.Name = "confirmPasswordLabel";
-            this.confirmPasswordLabel.Size = new System.Drawing.Size(233, 25);
+            this.confirmPasswordLabel.Size = new System.Drawing.Size(185, 20);
             this.confirmPasswordLabel.TabIndex = 24;
             this.confirmPasswordLabel.Text = "CONFIRM PASSWORD:";
             this.confirmPasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -215,9 +231,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Location = new System.Drawing.Point(72, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(425, 39);
+            this.label1.Size = new System.Drawing.Size(338, 31);
             this.label1.TabIndex = 25;
             this.label1.Text = "CREATE NEW ACCOUNT";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -229,9 +246,10 @@
             this.createAccountButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.createAccountButton.Font = new System.Drawing.Font("Calibri", 15F);
             this.createAccountButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(215)))), ((int)(((byte)(224)))));
-            this.createAccountButton.Location = new System.Drawing.Point(273, 468);
+            this.createAccountButton.Location = new System.Drawing.Point(205, 380);
+            this.createAccountButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.createAccountButton.Name = "createAccountButton";
-            this.createAccountButton.Size = new System.Drawing.Size(302, 57);
+            this.createAccountButton.Size = new System.Drawing.Size(226, 46);
             this.createAccountButton.TabIndex = 26;
             this.createAccountButton.Text = "CREATE NEW ACCOUNT";
             this.createAccountButton.UseVisualStyleBackColor = false;
@@ -244,9 +262,10 @@
             this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.backButton.Font = new System.Drawing.Font("Calibri", 15F);
             this.backButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(215)))), ((int)(((byte)(224)))));
-            this.backButton.Location = new System.Drawing.Point(12, 468);
+            this.backButton.Location = new System.Drawing.Point(9, 380);
+            this.backButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(248, 57);
+            this.backButton.Size = new System.Drawing.Size(186, 46);
             this.backButton.TabIndex = 27;
             this.backButton.Text = "BACK";
             this.backButton.UseVisualStyleBackColor = false;
@@ -254,10 +273,10 @@
             // 
             // SignupForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(40)))), ((int)(((byte)(62)))));
-            this.ClientSize = new System.Drawing.Size(593, 570);
+            this.ClientSize = new System.Drawing.Size(445, 463);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.createAccountButton);
             this.Controls.Add(this.label1);
@@ -278,6 +297,7 @@
             this.Controls.Add(this.lastNameText);
             this.Controls.Add(this.firstNameText);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SignupForm";
---
+++ University-advisor/Models/MarkerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_advisor.Models
+{
+    public class MarkerModel
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+    }
+}
